f54ce64404d4bc69d763bd78e4fc0f20e380cb1b
wilqor <wilqor@gmail.com>
2018-12-01 10:37:16 +0100

    Extracting EntryRecord

---
+++ core/src/main/java/introdb/heap/EntryRecord.java
@@ -0,0 +1,98 @@
+package introdb.heap;
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.Objects;
+
+final class EntryRecord {
+    private static final int RECORD_META_DATA_SIZE = 1 + (Short.SIZE / Byte.SIZE);
+    private static final byte DELETED_TRUE = 1;
+    private static final byte DELETED_FALSE = 0;
+
+    private final boolean deleted;
+    private final byte[] entryBytes;
+    private final Entry entry;
+
+    private EntryRecord(boolean deleted, byte[] entryBytes, Entry entry) {
+        this.deleted = deleted;
+        this.entryBytes = entryBytes;
+        this.entry = entry;
+    }
+
+    boolean isNotDeleted() {
+        return !deleted;
+    }
+
+    Entry getEntry() {
+        return entry;
+    }
+
+    int getRecordSize() {
+        return RECORD_META_DATA_SIZE + entryBytes.length;
+    }
+
+    EntryRecord toDeleted() {
+        return new EntryRecord(true, entryBytes, entry);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        var that = (EntryRecord) o;
+        return deleted == that.deleted &&
+                Objects.equals(entry, that.entry);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(deleted, entry);
+    }
+
+    @Override
+    public String toString() {
+        return "EntryRecord{" +
+                "deleted=" + deleted +
+                ", entry=" + entry +
+                '}';
+    }
+
+    void writeToBuffer(ByteBuffer buffer) {
+        buffer.put(deleted ? DELETED_TRUE : DELETED_FALSE);
+        buffer.putShort((short) entryBytes.length);
+        buffer.put(entryBytes);
+    }
+
+    static EntryRecord fromEntry(Entry entry) throws IOException {
+        byte[] entryBytes = bytesFromEntry(entry);
+        return new EntryRecord(false, entryBytes, entry);
+    }
+
+    private static byte[] bytesFromEntry(Entry entry) throws IOException {
+        var byteOutStr = new ByteArrayOutputStream();
+        var objOutStr = new ObjectOutputStream(byteOutStr);
+        objOutStr.writeObject(entry.key());
+        objOutStr.writeObject(entry.value());
+        return byteOutStr.toByteArray();
+    }
+
+    static EntryRecord fromBuffer(ByteBuffer byteBuffer) throws IOException, ClassNotFoundException {
+        byte deletedFlag = byteBuffer.get();
+        short entrySize = byteBuffer.getShort();
+        if (entrySize == 0) {
+            return null;
+        }
+        byte[] entryBytes = new byte[entrySize];
+        byteBuffer.get(entryBytes);
+        Entry entry = entryFromBytes(entryBytes);
+        return new EntryRecord(deletedFlag == DELETED_TRUE, entryBytes, entry);
+    }
+
+    private static Entry entryFromBytes(byte[] entryBytes) throws IOException, ClassNotFoundException {
+        var byteInStr = new ByteArrayInputStream(entryBytes);
+        var objInStr = new ObjectInputStream(byteInStr);
+        Serializable key = (Serializable) objInStr.readObject();
+        Serializable value = (Serializable) objInStr.readObject();
+        return new Entry(key, value);
+    }
+}
--- core/src/main/java/introdb/heap/UnorderedHeapFile.java
+++ core/src/main/java/introdb/heap/UnorderedHeapFile.java
@@ -1,6 +1,7 @@
 package introdb.heap;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.Serializable;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
@@ -26,35 +27,25 @@ class UnorderedHeapFile implements Store {
             long fullPagesCount = currentSize / pageSize;
             long remainingPageSize = currentSize % pageSize;
             long totalPagesCount = fullPagesCount + (remainingPageSize > 0 ? 1 : 0);
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(out);
-            oos.writeObject(entry.key());
-            oos.writeObject(entry.value());
-            byte[] entryBytes = out.toByteArray();
-            int entrySize = entryBytes.length;
-            // 1 byte flag + 2 bytes size = 3
-            int totalObjectSize = entrySize + 3;
-            if (totalObjectSize > pageSize) {
+            var record = EntryRecord.fromEntry(entry);
+            if (record.getRecordSize() > pageSize) {
                 throw new IllegalArgumentException("Too large object!");
             }
-            if ((pageSize - remainingPageSize) >= totalObjectSize) {
+            long newPosition;
+            if ((pageSize - remainingPageSize) >= record.getRecordSize()) {
                 // write on last page
-                fileChannel.position(currentSize);
+                newPosition = currentSize;
             } else {
                 // write to new page
                 if (totalPagesCount == maxNrPages) {
                     throw new IllegalArgumentException("Max number of pages reached!");
                 }
-                fileChannel.position(totalPagesCount * pageSize);
+                newPosition = totalPagesCount * pageSize;
             }
             ByteBuffer byteBuffer = ByteBuffer.allocate(pageSize);
-            // not deleted
-            byteBuffer.put((byte) 0);
-            // size
-            byteBuffer.putShort((short) entrySize);
-            byteBuffer.put(entryBytes);
+            record.writeToBuffer(byteBuffer);
             byteBuffer.flip();
-            fileChannel.write(byteBuffer);
+            fileChannel.write(byteBuffer, newPosition);
         }
     }
 
@@ -66,26 +57,12 @@ class UnorderedHeapFile implements Store {
                 // flip for reading
                 byteBuffer.flip();
                 while (byteBuffer.hasRemaining()) {
-                    byte deletedFlag = byteBuffer.get();
-                    short entrySize = byteBuffer.getShort();
-                    // page incomplete
-                    if (entrySize == 0) {
+                    var entryRecord = EntryRecord.fromBuffer(byteBuffer);
+                    if (entryRecord == null) {
                         break;
                     }
-                    if (deletedFlag == 1) {
-                        // skip
-                        byteBuffer.position(byteBuffer.position() + entrySize);
-                    } else {
-                        byte[] entryBytes = new byte[entrySize];
-                        byteBuffer.get(entryBytes);
-                        ByteArrayInputStream in = new ByteArrayInputStream(entryBytes);
-                        ObjectInputStream ois = new ObjectInputStream(in);
-                        // key
-                        Serializable currentKey = (Serializable) ois.readObject();
-                        if (key.equals(currentKey)) {
-                            // value
-                            return ois.readObject();
-                        }
+                    if (entryRecord.isNotDeleted() && entryRecord.getEntry().key().equals(key)) {
+                        return entryRecord.getEntry().value();
                     }
                 }
                 byteBuffer.clear();
@@ -102,31 +79,18 @@ class UnorderedHeapFile implements Store {
                 // flip for reading
                 byteBuffer.flip();
                 while (byteBuffer.hasRemaining()) {
-                    byte deletedFlag = byteBuffer.get();
-                    short entrySize = byteBuffer.getShort();
-                    // page incomplete
-                    if (entrySize == 0) {
+                    var entryRecord = EntryRecord.fromBuffer(byteBuffer);
+                    if (entryRecord == null) {
                         break;
                     }
-                    if (deletedFlag == 1) {
-                        // skip
-                        byteBuffer.position(byteBuffer.position() + entrySize);
-                    } else {
-                        byte[] entryBytes = new byte[entrySize];
-                        byteBuffer.get(entryBytes);
-                        ByteArrayInputStream in = new ByteArrayInputStream(entryBytes);
-                        ObjectInputStream ois = new ObjectInputStream(in);
-                        Serializable currentKey = (Serializable) ois.readObject();
-                        if (key.equals(currentKey)) {
-                            // entry value + flag + size
-                            int objectLength = entrySize + 3;
-                            byteBuffer.put(byteBuffer.position() - objectLength, (byte) 1);
-                            byteBuffer.flip();
-                            // move back the limit
-                            fileChannel.position(fileChannel.position() - objectLength);
-                            fileChannel.write(byteBuffer);
-                            return ois.readObject();
-                        }
+                    if (entryRecord.isNotDeleted() && key.equals(entryRecord.getEntry().key())) {
+                        var deleted = entryRecord.toDeleted();
+                        byteBuffer.position(byteBuffer.position() - deleted.getRecordSize());
+                        deleted.writeToBuffer(byteBuffer);
+                        byteBuffer.flip();
+                        fileChannel.position(fileChannel.position() - deleted.getRecordSize());
+                        fileChannel.write(byteBuffer);
+                        return deleted.getEntry().value();
                     }
                 }
                 byteBuffer.clear();
